c73c55f6559c8ca7596628b157fb123b64730b40
pankaj saini <java71932@gmail.com>
2026-08-19 11:00:08 +0530

    solve the problem of useStat hook

--- src/components/GalaryApplication/newproject.jsx
+++ src/components/GalaryApplication/newproject.jsx
@@ -3,19 +3,17 @@ import React, { useEffect, useState } from 'react'
 
 const newproject = () => {
     const [userdata, Setuserdata] = useState([])
-    const [index,SetIndex]=userState(1)
+    const [index, SetIndex] = useState(1)
 
-    function page_increate( index){
-        SetIndex(index+1)
-        
+    function page_increate(index) {
+        SetIndex(index + 1)
+
     }
-    function page_disreate(index)
-    {
-        if(index>1)
-        {
-            SetIndex(index-1)
+    function page_disreate(index) {
+        if (index > 1) {
+            SetIndex(index - 1)
         }
-        
+
     }
 
     async function getdata() {
@@ -37,30 +35,33 @@ const newproject = () => {
 
             <div className='flex flex-wrap  gap-4'>
                 {userdata.map(function (e) {
-                    return <div className=' overflow-hidden bg-gray-950 rounded-4xl border-amber-600 p-2'> <img className='w-40 h-40 ' src={e.download_url}></img>
+                    return <div key={e.id} className=' overflow-hidden bg-gray-950 rounded-4xl border-amber-600 p-2'> <img className='w-40 h-40 ' src={e.download_url}></img>
                         <h1 className='text-xl'>{e.author}</h1>
                     </div>
                 })}
             </div>
-             <button 
-          onClick={page_disreate}
-          disabled={index === 1}
-          className='bg-amber-600 disabled:bg-gray-700 text-white px-6 py-2 rounded-full font-bold cursor-pointer'
-        >
-          Prev
-        </button>
-        <span className='text-xl text-white font-bold'>Page {index}</span>
-        <button 
-          onClick={page_increate}
-          className='bg-amber-600 text-white px-6 py-2 rounded-full font-bold cursor-pointer'
-        >
-          Next
-        </button>
+            <div className='flex justify-center text-center'>
+            <button
+                onClick={()=>{page_disreate(index)}}
+                
+                className='bg-amber-600 disabled:bg-gray-700 text-white px-6 py-2 rounded-full font-bold cursor-pointer'
+            >
+                Prev
+            </button>
+            <p className='font-bold text-3xl font-bold text-blue-400 px-6 py-2'> {index}</p>
+            <button
+                onClick={()=>{page_increate(index)}}
+                className='bg-amber-600 text-white px-6 py-2 rounded-full font-bold cursor-pointer'
+            >
+                Next
+            </button>
+            </div>
+
 
         </div>
 
-        
-           
+
+
     )
 }
 
